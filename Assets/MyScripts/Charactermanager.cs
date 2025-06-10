5b71408d0ebd51206b573741dca5b14a656ba9f1
gregusia <gregorczykmartyna7@gmail.com>
2025-06-10 16:05:19 +0200

    stage

--- Assets/MyScripts/Charactermanager.cs
+++ Assets/MyScripts/Charactermanager.cs
@@ -14,8 +14,10 @@ public class CharacterManager : MonoBehaviour
 
     public void CharacterClicked(int id)
     {
+
         previewImage.sprite = characterSprites[id];
     }
+
 
 
 
@@ -23,12 +25,25 @@ public class CharacterManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SetupCharacters(); // aby przy starcie sceny wype³ni³o siê UI przygotowanymi elementami w tablicy characterSprites
+        CharacterClicked(0);//ustawienie bia³ego elementu Aliena na start
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetupCharacters()
     {
-        
+        for (int i = 0; i < characterSprites.Length; i++) //przejscie po rozmiarze tablicy zawierajacej wszystkich przygotowanych bohaterow
+        {
+             //Instantiate(characterOptionPrefab, characterContainer); // to spowoduje wype³nienie (sklonowanie) pierwszego elementu tablicy tyle razy ile jest obiektow w tablicy charaktersprites 
+
+            GameObject go = Instantiate(characterOptionPrefab, characterContainer);
+            CharacterOption characterOption = go.GetComponent<CharacterOption>(); // aby uzysakæ dostêp do metod obiektu characterOptions
+            //characterOption.Initialized(this, i);
+            //characterOption.enabled = true;
+
+
+            //GameObject go = Instantiate(characterOptionPrefab, charactersContainer);
+            //CharacterOption characterOption = go.GetComponent<CharacterOption>();
+            //characterOption.Initialize(this, i);
+        }
     }
 }
--- Assets/MyScripts/Options.cs
+++ Assets/MyScripts/Options.cs
@@ -8,13 +8,23 @@ public class CharacterOption : MonoBehaviour
 {
     public int id;
     public CharacterManager characterManager;
-
+    public Image characterImage;
+   
     public void OnClick()
     {
+     
         characterManager.CharacterClicked(id);
+
+        Debug.Log("Nacisnieto "+ id);
     }
-}
-{
+
+    public void Initialized(CharacterManager characterManager, int id) //aby zainicjowac odpowiednio tabele spritow 
+    {
+        this.characterManager = characterManager;
+        this.id = id;
+        characterImage.sprite = characterManager.characterSprites[id];
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
